06e723be7248159258ca16e92e55c9e28ff04276
Ankur Agrawal <ankur.agrawal@oracle.com>
2018-03-28 15:45:00 -0700

    Lab1 part III and IV

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -14,20 +14,96 @@ import (
 // suitable for passing to call(). registerChan will yield all
 // existing registered workers (if any) and new ones as they register.
 //
+
+type Task struct {
+	sync.Mutex
+	remainingTasks []int
+	scheduledTasks map[int]int //workerid to taskid map
+	taskDone *sync.Cond
+	ntasks int
+}
+
+func (tasks *Task) init(ntasks int) {
+	tasks.Lock()
+	defer tasks.Unlock()
+	tasks.ntasks = ntasks
+    tasks.remainingTasks = make([]int, 0)
+    tasks.scheduledTasks = make(map[int]int)
+    tasks.taskDone = sync.NewCond(tasks)
+    for i := 0; i < ntasks; i++ {
+    	    tasks.remainingTasks = append(tasks.remainingTasks, i)
+    }
+}
+
+func (tasks *Task) getNextFreeTask() int {
+	tasks.Lock()
+	defer tasks.Unlock()
+	if len(tasks.remainingTasks) == 0 {
+		return -1
+	}
+	freeTask := tasks.remainingTasks[0]
+	//tasks.remainingTasks = tasks.remainingTasks[1:]
+	return freeTask
+}
+
+func (tasks *Task) markAsScheduled(taskId int, workerId int) {
+	tasks.Lock()
+	defer tasks.Unlock()
+	t := make([]int, 0)
+	for _,remainingTask := range(tasks.remainingTasks) {
+		if remainingTask != taskId {
+			t = append(t, remainingTask)
+		}
+	}
+	tasks.remainingTasks = t
+	tasks.scheduledTasks[workerId] = taskId
+}
+
+func (tasks *Task) markTaskAsDone(taskId int, workerId int) {
+	tasks.Lock()
+	defer tasks.Unlock()
+	delete(tasks.scheduledTasks, workerId)
+	tasks.taskDone.Signal()
+}
+
+func (tasks *Task) allTasksDone() bool {
+	tasks.Lock()
+	defer tasks.Unlock()
+	if len(tasks.remainingTasks) == 0 && len(tasks.scheduledTasks) == 0 {
+		return true
+	}
+	return false
+}
+
+func (tasks *Task) markTaskAsFailed(taskId int, workerId int) {
+	tasks.Lock()
+	defer tasks.Unlock()
+	delete(tasks.scheduledTasks, workerId)
+	tasks.remainingTasks = append(tasks.remainingTasks, taskId)
+}
+
 type WorkersList struct {
 	sync.Mutex
-	workersArr []string
+	workersMap map[int]string //workerid to workerrpc
 	freeWorkers []int
+	numWorkers int
 	workerAvailable *sync.Cond
-	allDone *sync.Cond
+}
+
+func (workers *WorkersList) init() {
+	workers.workersMap = make(map[int]string)
+	workers.freeWorkers = make([]int, 0)
+	workers.numWorkers = 0
+	workers.workerAvailable = sync.NewCond(workers)
 }
 
 func (workers *WorkersList) addWorkers(registerChan chan string) {
 	for worker := range registerChan {
 		workers.Lock()
 		fmt.Println("took a lock for addworkers")
-		workers.workersArr = append(workers.workersArr, worker)
-		workers.freeWorkers = append(workers.freeWorkers, len(workers.workersArr)-1)
+		workers.workersMap[workers.numWorkers] = worker
+		workers.freeWorkers = append(workers.freeWorkers, workers.numWorkers)
+		workers.numWorkers++
 		fmt.Println("unlock for addworkers")
 		workers.Unlock()
 		workers.workerAvailable.Signal()
@@ -51,18 +127,27 @@ func (workers *WorkersList) getFreeWorkerIdx() int {
 	return freeWorkerIdx
 }
 
-func do_call(srv string, rpcname string, args interface{}, reply interface{}, workersList *WorkersList, freeWorker int) {
-	call(srv, rpcname, args, reply)
-	workersList.Lock()
-	fmt.Println("took a lock for do_call")
-	workersList.freeWorkers = append(workersList.freeWorkers, freeWorker)
-	fmt.Println("appended to workersList.freeWorkers, newsize: ", len(workersList.freeWorkers))
-	fmt.Println("unlock for do_call")
-	if len(workersList.freeWorkers) == len(workersList.workersArr) {
-		workersList.allDone.Signal()
+func (workers *WorkersList) markerWorkerAsFailed(workerId int) {
+	workers.Lock()
+	defer workers.Unlock()
+	delete(workers.workersMap, workerId)
+}
+
+func do_call(srv string, rpcname string, args interface{}, reply interface{},
+	workersList *WorkersList, freeWorker int, tasks *Task, taskId int) {
+	if call(srv, rpcname, args, reply) {
+		workersList.Lock()
+		fmt.Println("took a lock for do_call")
+		workersList.freeWorkers = append(workersList.freeWorkers, freeWorker)
+		fmt.Println("appended to workersList.freeWorkers, newsize: ", len(workersList.freeWorkers))
+		fmt.Println("unlock for do_call")
+		workersList.Unlock()
+		workersList.workerAvailable.Signal()
+		tasks.markTaskAsDone(taskId, freeWorker)
+	} else {
+		workersList.markerWorkerAsFailed(freeWorker)
+		tasks.markTaskAsFailed(taskId, freeWorker)
 	}
-	workersList.Unlock()
-	workersList.workerAvailable.Signal()
 }
 
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
@@ -85,13 +170,66 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
     when you get a worker from registerChan, assign it a task based on phase, update the datastructure, and wait for completion
     when all tasks are completed, return
     */
-    //workerMap := make(map[string]chan)
+    
+    tasks := new(Task)
+    tasks.init(ntasks)
+    
     workersList := new(WorkersList)
-    workersList.workerAvailable = sync.NewCond(workersList)
-    workersList.allDone = sync.NewCond(workersList)
+    workersList.init()
     go workersList.addWorkers(registerChan)
+    
+    for !tasks.allTasksDone() {
+    	    taskId := tasks.getNextFreeTask()
+    	    if taskId == -1 {
+    	    	    tasks.Lock()
+    	    	    tasks.taskDone.Wait()
+    	    	    tasks.Unlock()
+    	    	    continue
+    	    }
+    	    workerId := workersList.getFreeWorkerIdx()
+    	    if workerId == -1 {
+    	    	    workersList.Lock()
+    	    	    workersList.workerAvailable.Wait()
+    	    	    workersList.Unlock()
+    	    	    continue
+    	    }
+    	    var taskArgs DoTaskArgs
+        taskArgs.JobName = jobName
+        taskArgs.File = mapFiles[taskId]
+        fmt.Println("**5")
+        taskArgs.NumOtherPhase = n_other
+        taskArgs.Phase = phase
+        taskArgs.TaskNumber = taskId
+        fmt.Println("scheduling task ", taskId, " on worker ", workerId)
+        tasks.markAsScheduled(taskId, workerId)
+    	    go do_call(workersList.workersMap[workerId], "Worker.DoTask", taskArgs, nil, workersList, workerId, tasks, taskId)
+    }
+    /*
     fmt.Println("**1")
     donetasks := 0
+    for len(tasks.doneTasks) < len(tasks.taskIds) {
+    	    if len(tasks.scheduledTasks) > 0 && len(tasks.taskIds) == len(tasks.doneTasks) + len(tasks.scheduledTasks) {
+	    	    workersList.Lock()
+	    	    workersList.taskDone.Wait()
+	    	    workersList.Unlock()
+	    	    continue
+    	    }
+    	    freeWorker := workersList.getFreeWorkerIdx()
+    	    if freeWorker == -1 {
+    	    	    workersList.Lock()
+    	    	    workersList.workerAvailable.Wait()
+    	    	    workersList.Unlock()
+    	    	    continue
+    	    }
+    	    workersList.Lock()
+    	    var taskArgs DoTaskArgs
+        taskArgs.JobName = jobName
+        taskArgs.File = mapFiles[donetasks]
+        taskArgs.NumOtherPhase = n_other
+        taskArgs.Phase = phase
+        taskArgs.TaskNumber = donetasks
+        tasks.scheduledTasks[
+    }
     for donetasks < ntasks {
     	    fmt.Println("**2")
     	    fmt.Println("number of donetasks are: ", donetasks, " number of ntasks: ", ntasks)
@@ -121,11 +259,12 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
         fmt.Println("**7")
     }
     
-    for len(workersList.freeWorkers) < len(workersList.workersArr) {
+    for len(workersList.freeWorkers) < workersList.healthyWorkers {
        	workersList.Lock()
     	    workersList.allDone.Wait()
     	    workersList.Unlock()
     }
+    */
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	//
