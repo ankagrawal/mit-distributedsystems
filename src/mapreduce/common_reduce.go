10d9ca33dd254ca674a2b871a7fbad87d9842eeb
Ankur Agrawal <ankur.agrawal@oracle.com>
2018-03-28 15:53:37 -0700

    Cleanup

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,7 +1,6 @@
 package mapreduce
 
 import (
-	"fmt"
 	"encoding/json"
 	"sort"
 	"os"
@@ -15,20 +14,11 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	fmt.Println("in reduce")
 	keyvaluesMap := make(map[string][]string)
 	var result []KeyValue
 	for i := 0; i < nMap; i++ {
-		fmt.Println("reading ", reduceName(jobName, i, reduceTask))
-		/*b, err := ioutil.ReadFile(reduceName(jobName, i, reduceTask))
-		fmt.Println("size read from file", len(b))
-		if err != nil {
-			fmt.Println("error", err, "while reading file: ", reduceName(jobName, 0, 1))
-			panic(err)
-	    }*/
 		file,err1 := os.Open(reduceName(jobName, i, reduceTask))
 		if err1 != nil {
-			fmt.Println("error", err1, "while reading file: ", reduceName(jobName, 0, 1))
 			panic(err1)
 	    }
 		decoder := json.NewDecoder(file)
@@ -39,17 +29,14 @@ func doReduce(
 			if decErr == io.EOF {
 				break
 			} else if decErr != nil {
-				fmt.Println("Error ", decErr, " while decoding ctr", ctr)
 				panic(decErr)
 			}
 			ctr++
 			keyvaluesMap[kv.Key] = append(keyvaluesMap[kv.Key], kv.Value)
 		}
-		fmt.Println("number of keyvalues read in reduce ", ctr)
 		file.Close()
 	}
 	for k := range keyvaluesMap {
-		//fmt.Println("going to reduce key: ", k, "value: ", keyvaluesMap[k])
 		resultVal := reduceF(k, keyvaluesMap[k])
 		var kv KeyValue
 		kv.Key = k
@@ -60,57 +47,7 @@ func doReduce(
 	f,_ := os.Create(outFile)
 	enc := json.NewEncoder(f)
 	for _, kv := range result {
-		//fmt.Println("result key: ", kv.Key, " value: ", kv.Value)
 		enc.Encode(kv)
 	}
 	f.Close()
-	/*
-	fmt.Println(reduceName(jobName, 0, 1))
-	b, err := ioutil.ReadFile(reduceName(jobName, 0, 1))
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("length of byte array", len(b))
-	var keyvalues []KeyValue
-	json.NewDecoder(bytes.NewReader(b)).Decode(&keyvalues)
-	fmt.Println("print length of struct")
-	fmt.Println(len(keyvalues))
-	*/
-	//
-	// doReduce manages one reduce task: it should read the intermediate
-	// files for the task, sort the intermediate key/value pairs by key,
-	// call the user-defined reduce function (reduceF) for each key, and
-	// write reduceF's output to disk.
-	//
-	// You'll need to read one intermediate file from each map task;
-	// reduceName(jobName, m, reduceTask) yields the file
-	// name from map task m.
-	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
-	// reduceF() is the application's reduce function. You should
-	// call it once per distinct key, with a slice of all the values
-	// for that key. reduceF() returns the reduced value for that key.
-	//
-	// You should write the reduce output as JSON encoded KeyValue
-	// objects to the file named outFile. We require you to use JSON
-	// because that is what the merger than combines the output
-	// from all the reduce tasks expects. There is nothing special about
-	// JSON -- it is just the marshalling format we chose to use. Your
-	// output code will look something like this:
-	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Your code here (Part I).
-	//
 }
--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -100,11 +100,9 @@ func (workers *WorkersList) init() {
 func (workers *WorkersList) addWorkers(registerChan chan string) {
 	for worker := range registerChan {
 		workers.Lock()
-		fmt.Println("took a lock for addworkers")
 		workers.workersMap[workers.numWorkers] = worker
 		workers.freeWorkers = append(workers.freeWorkers, workers.numWorkers)
 		workers.numWorkers++
-		fmt.Println("unlock for addworkers")
 		workers.Unlock()
 		workers.workerAvailable.Signal()
 	}
@@ -112,17 +110,13 @@ func (workers *WorkersList) addWorkers(registerChan chan string) {
 
 func (workers *WorkersList) getFreeWorkerIdx() int {
 	workers.Lock()
-	fmt.Println("took a lock for getFreeWorkerIdx")
 	//defer workers.Unlock()
 	if len(workers.freeWorkers) == 0 {
-		fmt.Println("unlock for getFreeWorkerIdx, returning -1")
 		workers.Unlock()
 		return -1
 	}
 	freeWorkerIdx := workers.freeWorkers[0]
 	workers.freeWorkers = workers.freeWorkers[1:]
-	fmt.Println("remvoed from workersList.freeWorkers, newsize: ", len(workers.freeWorkers))
-	fmt.Println("unlock for getFreeWorkerIdx")
 	workers.Unlock()
 	return freeWorkerIdx
 }
@@ -137,10 +131,7 @@ func do_call(srv string, rpcname string, args interface{}, reply interface{},
 	workersList *WorkersList, freeWorker int, tasks *Task, taskId int) {
 	if call(srv, rpcname, args, reply) {
 		workersList.Lock()
-		fmt.Println("took a lock for do_call")
 		workersList.freeWorkers = append(workersList.freeWorkers, freeWorker)
-		fmt.Println("appended to workersList.freeWorkers, newsize: ", len(workersList.freeWorkers))
-		fmt.Println("unlock for do_call")
 		workersList.Unlock()
 		workersList.workerAvailable.Signal()
 		tasks.markTaskAsDone(taskId, freeWorker)
@@ -151,7 +142,6 @@ func do_call(srv string, rpcname string, args interface{}, reply interface{},
 }
 
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
-	fmt.Println("in schedule")
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
@@ -196,79 +186,12 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
     	    var taskArgs DoTaskArgs
         taskArgs.JobName = jobName
         taskArgs.File = mapFiles[taskId]
-        fmt.Println("**5")
         taskArgs.NumOtherPhase = n_other
         taskArgs.Phase = phase
         taskArgs.TaskNumber = taskId
-        fmt.Println("scheduling task ", taskId, " on worker ", workerId)
         tasks.markAsScheduled(taskId, workerId)
     	    go do_call(workersList.workersMap[workerId], "Worker.DoTask", taskArgs, nil, workersList, workerId, tasks, taskId)
     }
-    /*
-    fmt.Println("**1")
-    donetasks := 0
-    for len(tasks.doneTasks) < len(tasks.taskIds) {
-    	    if len(tasks.scheduledTasks) > 0 && len(tasks.taskIds) == len(tasks.doneTasks) + len(tasks.scheduledTasks) {
-	    	    workersList.Lock()
-	    	    workersList.taskDone.Wait()
-	    	    workersList.Unlock()
-	    	    continue
-    	    }
-    	    freeWorker := workersList.getFreeWorkerIdx()
-    	    if freeWorker == -1 {
-    	    	    workersList.Lock()
-    	    	    workersList.workerAvailable.Wait()
-    	    	    workersList.Unlock()
-    	    	    continue
-    	    }
-    	    workersList.Lock()
-    	    var taskArgs DoTaskArgs
-        taskArgs.JobName = jobName
-        taskArgs.File = mapFiles[donetasks]
-        taskArgs.NumOtherPhase = n_other
-        taskArgs.Phase = phase
-        taskArgs.TaskNumber = donetasks
-        tasks.scheduledTasks[
-    }
-    for donetasks < ntasks {
-    	    fmt.Println("**2")
-    	    fmt.Println("number of donetasks are: ", donetasks, " number of ntasks: ", ntasks)
-    	    fmt.Println("number of workers: ", len(workersList.workersArr))
-    	    fmt.Println("number of freeworkers: ", len(workersList.freeWorkers))
-    	    freeWorker := workersList.getFreeWorkerIdx()
-    	    if freeWorker == -1 {
-    	    	    fmt.Println("**3")
-    	    	    workersList.Lock()
-    	    	    fmt.Println("took a lock for schedule")
-    	    	    workersList.workerAvailable.Wait()
-    	    	    fmt.Println("unlock for schedule")
-    	    	    workersList.Unlock()
-    	    	    continue
-    	    }
-    	    fmt.Println("**4")
-    	    var taskArgs DoTaskArgs
-        taskArgs.JobName = jobName
-        taskArgs.File = mapFiles[donetasks]
-        fmt.Println("**5")
-        taskArgs.NumOtherPhase = n_other
-        taskArgs.Phase = phase
-        taskArgs.TaskNumber = donetasks
-        donetasks++
-        fmt.Println("**6")
-        go do_call(workersList.workersArr[freeWorker], "Worker.DoTask", taskArgs, nil, workersList, freeWorker)
-        fmt.Println("**7")
-    }
-    
-    for len(workersList.freeWorkers) < workersList.healthyWorkers {
-       	workersList.Lock()
-    	    workersList.allDone.Wait()
-    	    workersList.Unlock()
-    }
-    */
-	// All ntasks tasks have to be scheduled on workers. Once all tasks
-	// have completed successfully, schedule() should return.
-	//
-	// Your code here (Part III, Part IV).
-	//
+ 
 	fmt.Println("Schedule: %v done\n", phase)
 }
